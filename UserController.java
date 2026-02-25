e7c9cbf3ad7d66bcf792f635c07e47350d3fb8df
sriharshaparigi <harshaparigi@gmail.com>
2026-02-25 14:43:21 +0530

    Add files via upload

---
+++ UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    public UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+}
